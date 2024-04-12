7072ab1bb99e118f8503f7b8e800381c6fdbac31
Renzo Jared <renzojared_lm@hotmail.com>
2024-04-11 21:07:57 -0500

    remove northwind membership from principal to other solution

--- NorthWind/NorthWind.Membership.Entities/DTOs/UserRegistration/UserRegistrationDto.cs
+++
@@ -1,9 +0,0 @@
-namespace NorthWind.Membership.Entities.DTOs.UserRegistration;
-
-public class UserRegistrationDto(string email, string password, string firstName, string lastName)
-{
-    public string Email => email;
-    public string Password => password;
-    public string FirstName => firstName;
-    public string LastName => lastName;
-}
--- NorthWind/NorthWind.Membership.Entities/Validators/UserRegistration/UserRegistrationDtoValidator.cs
+++
@@ -1,6 +0,0 @@
-namespace NorthWind.Membership.Entities.Validators.UserRegistration;
-
-public class UserRegistrationDtoValidator
-{
-    //TODO: Implement
-}
